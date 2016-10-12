b1d7e4f94f21e2b3c5c0149dab71a38b2ba4025c
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-12 11:54:30 +0100

    Add player names to session

--- Battle/app.rb
+++ Battle/app.rb
@@ -2,15 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  enable :session
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
-  post '/battle' do
-    @player_1 = params[:player_1_name]
-    @player_2 = params[:player_2_name]
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1_name]
+    @player_2 = session[:player_2_name]
     erb :play
   end
 
