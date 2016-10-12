cc9d3aadf947ee9d6ee2134c60504ad212225df1
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-12 12:32:52 +0100

    Add web_helpers.rb

--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature "Entering Players" do
   scenario "Players enter their names, submit and see them" do
-    visit "/"
-    fill_in "player_1_name", :with => "Kyaw"
-    fill_in "player_2_name", :with => "Laura"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_text("Kyaw vs. Laura")
   end
 end
