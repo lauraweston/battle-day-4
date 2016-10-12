53ae20af7349b42ee7903370a4e9e881070841be
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-12 12:22:43 +0100

    Refactoer

--- Battle/spec/features/hit_points_spec.rb
+++ Battle/spec/features/hit_points_spec.rb
@@ -1,11 +1,9 @@
-feature "player hit points" do
-  scenario "Player 1 can see Player 2's hit points" do
+feature "View hit points" do
+  scenario "see Player 2 hit points" do
     visit "/"
     fill_in "player_1_name", :with => "Kyaw"
     fill_in "player_2_name", :with => "Laura"
     click_button "Submit"
-    within "p" do
-    expect(page).to have_text("Laura (100/100 HP)")
-    end
+    expect(page).to have_text("Laura: 100HP")
   end
 end
